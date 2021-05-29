1cdd64d47a5419307112ca473916e226553ddbb5
Khushboo2506 <singhkhusboo.256@gmail.com>
2021-05-29 13:58:23 +0530

    secong commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -25,6 +25,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Khushboo Singh</h1>
+          <p id="text">An enthusiast who loves world-changing computer science technologies,</p><p id="text">and is dedicated to learning new things and languages.</p><br></br>
           <h3><span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
